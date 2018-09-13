9be6d2a8bbd517a566e6f62c758c1e8d6ff321e4
Dinara Faizullina <imfd@protonmail.com>
2018-09-13 17:01:19 +0300

    Interpolation class refactoring

--- InterpolationApp/InterpolationApp/Interpolation.cs
+++ InterpolationApp/InterpolationApp/Interpolation.cs
@@ -10,32 +10,32 @@ namespace InterpolationApp
         /// <summary>
         /// Список узлов интерполяции
         /// </summary>
-        public List<double> Points { get; set; }
+        //public List<double> Points { get; set; }
 
         /// <summary>
         /// Список значений функции в узлах интерполяции
         /// </summary>
-        public List<double> Values { get; set; }
+        //public List<double> Values { get; set; }
 
         /// <summary>
         /// Шаг для узлов интерполяции
         /// </summary>
-        public double Step { get; set; }
+        //public double Step { get; set; }
 
         /// <summary>
         /// Отрезок, из которого берутся равностоящие значения узлов с шагом Step
         /// </summary>
-        public Tuple<double, double> Segment { get; set; }
+        //public Tuple<double, double> Segment { get; set; }
 
         /// <summary>
         /// Точность вычисления значений функции
         /// </summary>
-        public double Precision { get; set; }
+        //public double Precision { get; set; }
 
         /// <summary>
-        /// Наивысший порядок для конечных разностей дельта
+        /// 
         /// </summary>
-        public int DeltaDegree { get; set; }
+        //public int DeltaDegree { get; set; }
 
         /// <summary>
         /// Делегат интерполируемой функции
@@ -47,14 +47,12 @@ namespace InterpolationApp
         /// <summary>
         /// Списки конечных разностей порядка до DeltaDegree
         /// </summary>
-        public List<List<double>> Deltas { get; set; }
+        //public List<List<double>> Deltas { get; set; }
 
         /// <summary>
         /// Интерполируемая функция
         /// </summary>
-        public FunctionDelegate Function;
-
-        public FunctionDelegate NFunction;
+        //public FunctionDelegate Function;
 
         public Interpolation(FunctionDelegate function, 
                              Tuple<double, double> segment, 
@@ -62,63 +60,88 @@ namespace InterpolationApp
                              double precision, 
                              int deltaDegree)
         {
-            Function = function;
-            Segment = segment;
-            Step = step;
-            Precision = precision;
-            DeltaDegree = deltaDegree;
+            //Function = function;
+            //Segment = segment;
+            //Step = step;
+            //Precision = precision;
+            //DeltaDegree = deltaDegree;
 
-            GeneratePoints();
-            GenerateValues();
-            GenerateDeltas();
+            //GeneratePoints();
+            //GenerateValues();
+            //GenerateDeltas();
         }
 
-
-        private void GeneratePoints()
+        /// <summary>
+        /// Вычисление координат равностоящих интерполяционных узлов
+        /// </summary>
+        /// <param name="segment">Пара из координат начальной и конечной точек отрезка</param>
+        /// <param name="step">Шаг для равностоящих узлов</param>
+        /// <returns>Список вещественных узлов из отрезка segment, отстоящих друг от друга на step</returns>
+        private List<double> GetPoints(Tuple<double, double> segment, double step)
         {
-            Points = new List<double>();
+            var points = new List<double>();
 
-            var n = Math.Round((Segment.Item2 - Segment.Item1) / Step) + 1;
+            var n = Math.Round((segment.Item2 - segment.Item1) / step) + 1;
+
 
             for (int i = 0; i < n; i++)
             {
-                double p = Segment.Item1 + Step * i;
-                Points.Add(p);
+                double p = segment.Item1 + step * i;
+                points.Add(p);
             }
+
+            return points;
         }
 
-        private void GenerateValues()
+        /// <summary>
+        /// Вычисление значений функции в интерполяционных узлах
+        /// </summary>
+        /// <param name="function">Интерполируемая функция f: R -> R</param>
+        /// <param name="points">Список узлов - параметров функции</param>
+        /// <param name="precision">Точность вычисления</param>
+        /// <returns>Список значений функции в узлах</returns>
+        private List<double> GetValues(FunctionDelegate function, List<double> points, double precision)
         {
-            Values = new List<double>();
-
-            foreach (double p in Points)
-            {
-                Values.Add(TruncateValue(Precision, Function(p)));
-            }
+            var values = new List<double>();
+            points.ForEach(p => values.Add((Truncate(precision, function(p)))));
+            return values;
         }
 
-        private void GenerateDeltas()
+        /// <summary>
+        /// Вычисление конечных разностей
+        /// </summary>
+        /// <param name="values">Список значений функции в узлах интерполяции</param>
+        /// <param name="degree">Необходимый наивысший порядок для конечных разностей</param>
+        /// <returns>Список из разностей порядков 0..degree</returns>
+        private List<List<double>> GetDeltas(List<double> values, int degree)
         {
-            Deltas = new List<List<double>>();
+            var deltas = new List<List<double>>();
 
             var deltas0 = new List<double>();
-            for (int i = 0; i < Values.Count - 1; i++)
+            for (int i = 0; i < values.Count - 1; i++)
             {
-                deltas0.Add(Values[i + 1] - Values[i]);
+                deltas0.Add(values[i + 1] - values[i]);
             }
-            Deltas.Add(deltas0);
+            deltas.Add(deltas0);
 
-            for (int i = 1; i < DeltaDegree; i++)
+            for (int i = 1; i < degree; i++)
             {
                 var deltasI = new List<double>();
-                for (int j = 0; j < Deltas[i - 1].Count - 1; j++)
+                for (int j = 0; j < deltas[i - 1].Count - 1; j++)
                 {
-                    deltasI.Add((Deltas[i - 1][j + 1] - Deltas[i - 1][j]));
+                    deltasI.Add((deltas[i - 1][j + 1] - deltas[i - 1][j]));
                 }
-                Deltas.Add(deltasI);
+                deltas.Add(deltasI);
             }
+
+            return deltas;
         }
         
+        /// <summary>
+        /// Генерирование списка из делегатов вспомогательных рекурсивных функций Nk(t)
+        /// </summary>
+        /// <param name="k">Необходимый наивысший порядок для системы функций</param>
+        /// <returns>Список делегатов функций double -> double</returns>
         private List<FunctionDelegate> GenerateNFunctions(int k)
         {
             var NFunctions = new List<FunctionDelegate>
@@ -134,10 +157,22 @@ namespace InterpolationApp
             return NFunctions;
         }
 
-        public double TruncateValue(double precision, double value)
+        /// <summary>
+        /// Округление значений с необходимой точностью 
+        /// </summary>
+        /// <param name="precision">Число знаков после запятой</param>
+        /// <param name="value">Округляемое значение</param>
+        /// <returns>Округленное значение</returns>
+        public double Truncate(double precision, double value)
         {
-            double revPrecision = 1 / Precision;
+            double revPrecision = 1 / precision;
             return Math.Truncate(revPrecision * value) / revPrecision;
         }
+
+        //public double Interpolate(double x, int n)
+        //{
+        //    var NFunctions = GenerateNFunctions(n);
+
+        //}
     }
 }
