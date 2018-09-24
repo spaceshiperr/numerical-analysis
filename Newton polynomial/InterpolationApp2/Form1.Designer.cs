d6d977ed2f32815ab872780e9ad021cdc2d3def5
Dinara Faizullina <imfd@protonmail.com>
2018-09-25 00:02:53 +0300

    Added new interpolation task for random points
    All in one source file. How awesome is that

---
+++ Newton polynomial/InterpolationApp2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace InterpolationApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.diffGrid = new System.Windows.Forms.DataGridView();
+            this.ColumnX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDiff1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDiff2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.resultGrid = new System.Windows.Forms.DataGridView();
+            this.ColumnI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.diffGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // diffGrid
+            // 
+            this.diffGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.diffGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.diffGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnX,
+            this.ColumnF,
+            this.ColumnDiff1,
+            this.ColumnDiff2,
+            this.Column3});
+            this.diffGrid.Dock = System.Windows.Forms.DockStyle.Top;
+            this.diffGrid.Location = new System.Drawing.Point(0, 0);
+            this.diffGrid.Name = "diffGrid";
+            this.diffGrid.Size = new System.Drawing.Size(800, 235);
+            this.diffGrid.TabIndex = 0;
+            // 
+            // ColumnX
+            // 
+            this.ColumnX.HeaderText = "x";
+            this.ColumnX.Name = "ColumnX";
+            // 
+            // ColumnF
+            // 
+            this.ColumnF.HeaderText = "f(x)";
+            this.ColumnF.Name = "ColumnF";
+            // 
+            // ColumnDiff1
+            // 
+            this.ColumnDiff1.HeaderText = "р.р. 1-го п.";
+            this.ColumnDiff1.Name = "ColumnDiff1";
+            // 
+            // ColumnDiff2
+            // 
+            this.ColumnDiff2.HeaderText = "р.р. 2-го п.";
+            this.ColumnDiff2.Name = "ColumnDiff2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "р.р. 3-го п.";
+            this.Column3.Name = "Column3";
+            // 
+            // resultGrid
+            // 
+            this.resultGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.resultGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnI,
+            this.Column0,
+            this.Column1,
+            this.Column2,
+            this.Column4});
+            this.resultGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.resultGrid.Location = new System.Drawing.Point(0, 241);
+            this.resultGrid.Name = "resultGrid";
+            this.resultGrid.Size = new System.Drawing.Size(800, 161);
+            this.resultGrid.TabIndex = 1;
+            // 
+            // ColumnI
+            // 
+            this.ColumnI.HeaderText = "i";
+            this.ColumnI.Name = "ColumnI";
+            // 
+            // Column0
+            // 
+            this.Column0.HeaderText = "0";
+            this.Column0.Name = "Column0";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "3";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 402);
+            this.Controls.Add(this.resultGrid);
+            this.Controls.Add(this.diffGrid);
+            this.Name = "Form1";
+            this.Text = "Интерполяционный многочлен в форме Ньютона";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.diffGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView diffGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDiff1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDiff2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridView resultGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ Newton polynomial/InterpolationApp2/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace InterpolationApp2
+{
+    public partial class Form1 : Form
+    {
+        //private List<double> points = new List<double>{ -0.6, -0.5, -0.3, -0.2, -0.1, 0 };
+        //private double intPoint = -0.4;
+        //private double functionValue = 0.8;
+        //private FunctionDelegate function = Math.Cos;
+
+        private List<double> Points = new List<double> { -2, 0, 1, 3, 4, 5 };
+        private double IntPoint = 2;
+        private double FunctionValue = 10;
+        private FunctionDelegate Function = (x => Math.Pow(x, 3) + 2);
+        private int Degree = 4;
+
+        private delegate double FunctionDelegate(double x);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var values = Points.Select(x => Function(x)).ToList();           
+
+            var diffList = GetDividedDifferences(Points, values);
+
+            FillDiffGrid(Points, values, diffList);
+            var newPoints = ChooseClosePoints(Points, IntPoint, Degree);
+            var newValues = newPoints.Select(x => Function(x)).ToList();
+
+            var interpolatedValues = new List<double>();
+            for (int i = 0; i < Degree; i++)
+            {
+                var value = Interpolate(IntPoint, newPoints, newValues, i);
+                interpolatedValues.Add(value);
+            }
+
+            var errors = GetCalculationError(FunctionValue, interpolatedValues);
+
+            FillResultGrid(newPoints, interpolatedValues, errors);
+        }
+
+        private void FillDiffGrid(List<double> points, List<double> values, List<List<double>> diffList)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                diffGrid.Rows.Add(points[i], values[i]);
+                diffGrid.Rows.Add();
+            }
+                
+            int rowStartIndex = 1;
+            int columnIndex = 2;
+
+            for (int i = columnIndex; i < diffGrid.ColumnCount; i++)
+            {
+                int rowIndex = rowStartIndex;
+                foreach (var diff in diffList[i - 2])
+                {
+                    diffGrid[i, rowIndex].Value = diff;
+                    rowIndex += 2;
+                }
+
+                rowStartIndex++;
+            }
+        }
+
+        private void FillResultGrid(List<double> points, List<double> interpolatedValues, List<double> errors)
+        {
+            resultGrid.Rows.Add("Узлы");
+            resultGrid.Rows.Add("P_i(x)");
+            resultGrid.Rows.Add("f(x) - P_i(x)");
+            resultGrid.Rows.Add("M_{i+1}");
+            resultGrid.Rows.Add("R_i(x)");
+
+            for (int i = 1; i < resultGrid.ColumnCount; i++)
+            {
+                resultGrid[i, 0].Value = points[i - 1];
+                resultGrid[i, 1].Value = interpolatedValues[i - 1];
+                resultGrid[i, 2].Value = errors[i - 1];
+
+            }
+        }
+
+        private List<List<double>> GetDividedDifferences(List<double> points, List<double> values)
+        {
+            var diffs = new List<List<double>>();
+
+            var diffs1 = new List<double>();
+            for (int i = 1; i < points.Count; i++)
+            {
+                var diff = (values[i] - values[i - 1]) / (points[i] - points[i - 1]);
+                diffs1.Add(diff);
+            }
+
+            diffs.Add(diffs1);
+
+            int diffNumber = points.Count / 2 - 1;
+            
+            for (int i = 0; i <= diffNumber; i++)
+            {
+                int beginPoint = 0;
+                int endPoint = i + 2;
+                var diffsN = new List<double>();
+
+                for (int j = 1; j < diffs[diffs.Count - 1].Count; j++)
+                {
+                    var diff = (diffs[diffs.Count - 1][j] - diffs[diffs.Count - 1][j - 1]) / (points[endPoint] - points[beginPoint]);
+                    diffsN.Add(diff);
+                    beginPoint++;
+                    endPoint++;
+                }
+
+                diffs.Add(diffsN);
+            }
+
+            return diffs;
+        }
+
+        private List<double> ChooseClosePoints(List<double> points, double intPoint, int count)
+        {
+            var newPoints = new List<double>();
+            
+            for (int i = 0; i < count; i++)
+            {
+                var distances = new List<double>();
+                points.ForEach(x => distances.Add(Math.Abs(x - intPoint)));
+                var minDistanceIndex = distances.IndexOf(distances.Min());
+
+                newPoints.Add(points[minDistanceIndex]);
+                points.Remove(points[minDistanceIndex]);
+            }
+
+            return newPoints;
+        }
+
+        private List<double> GetCalculationError(double value, List<double> calculatedValues)
+        {
+            return calculatedValues.Select(calcValue => value - calcValue).ToList();
+        }
+
+        private double Interpolate(double intPoint, List<double> points, List<double> values, int degree)
+        {
+            if (degree == 0)
+                return values[0];
+
+            var result = values[0];
+            var diffs = GetDividedDifferences(points, values);
+
+            for (int i = 0; i < degree; i++)
+            {
+                double product = 1;
+                for (int j = 0; j <= i; j++)
+                {
+                    product *= intPoint - points[j];
+                }
+
+                result += diffs[i][0] * product;
+            }
+
+            return result;
+        }
+
+        //найти Mi+1 как максимум производной и погрешность
+    }
+}
