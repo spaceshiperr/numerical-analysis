4e52ad0a45aafe258bf8b48959c4b50f5933ffbe
Dinara Faizullina <imfd@protonmail.com>
2018-09-25 14:22:57 +0300

    Created a separate interpolation class
    I guess that's good enough

--- Newton polynomial/InterpolationApp2/Form1.cs
+++ Newton polynomial/InterpolationApp2/Form1.cs
@@ -1,5 +1,4 @@
-﻿using MathNet.Numerics.Interpolation;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -9,18 +8,20 @@ namespace InterpolationApp2
 {
     public partial class Form1 : Form
     {
-        //private List<double> points = new List<double>{ -0.6, -0.5, -0.3, -0.2, -0.1, 0 };
-        //private double intPoint = -0.4;
-        //private double functionValue = 0.8;
-        //private FunctionDelegate function = Math.Cos;
+        // input data
 
-        private List<double> Points = new List<double> { -2, 0, 1, 3, 4, 5 };
-        private double IntPoint = 2;
-        private double FunctionValue = 10;
-        private FunctionDelegate Function = (x => Math.Pow(x, 3) + 2);
+        private List<double> Points = new List<double> { -0.6, -0.5, -0.3, -0.2, -0.1, 0 };
+        private double IntPoint = -0.4;
+        private double FunctionValue = 0.8;
+        private Interpolation.FunctionDelegate Function = Math.Cos;
+
+        //private List<double> Points = new List<double> { -2, 0, 1, 3, 4, 5 };
+        //private double IntPoint = 2;
+        //private double FunctionValue = 10;
+        //private Interpolation.FunctionDelegate Function = (x => Math.Pow(x, 3) + 2);
+
         private int Degree = 4;
-
-        private delegate double FunctionDelegate(double x);
+        private double Step = 0.1;
 
         public Form1()
         {
@@ -29,35 +30,24 @@ namespace InterpolationApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            var values = Points.Select(x => Function(x)).ToList();           
-
-            var diffList = GetDividedDifferences(Points, values);
+            var values = Points.Select(x => Function(x)).ToList();          
+            var diffList = Interpolation.GetDividedDifferences(Points, values);
 
             FillDiffGrid(Points, values, diffList);
-            var newPoints = ChooseClosePoints(Points, IntPoint, Degree);
+
+            var newPoints = Interpolation.ChooseClosePoints(Points, IntPoint, Degree);
             var newValues = newPoints.Select(x => Function(x)).ToList();
+            var interpolatedValues = Enumerable.Range(0, Degree).Select(x => Interpolation.Interpolate(IntPoint, newPoints, newValues, x)).ToList();
+            var errors = Interpolation.GetCalculationError(FunctionValue, interpolatedValues);
+            var maxModules = Enumerable.Range(0, Degree).Select(degree => Interpolation.EvaluateProductModule(newPoints, newValues, IntPoint, degree + 1, Step)).ToList();
+            var errorEstimations = Enumerable.Range(0, Degree).Select(degree => Interpolation.EstimateError(newPoints, newValues, IntPoint, degree, Step)).ToList();
 
-            var interpolatedValues = new List<double>();
-            for (int i = 0; i < Degree; i++)
-            {
-                var value = Interpolate(IntPoint, newPoints, newValues, i);
-                interpolatedValues.Add(value);
-            }
-
-            var errors = GetCalculationError(FunctionValue, interpolatedValues);
-
-            FillResultGrid(newPoints, interpolatedValues, errors);
-
-            var res = Differentiation(newPoints, newValues, IntPoint, 2);
+            FillResultGrid(newPoints, interpolatedValues, errors, maxModules, errorEstimations);
         }
 
         private void FillDiffGrid(List<double> points, List<double> values, List<List<double>> diffList)
         {
-            for (int i = 0; i < points.Count; i++)
-            {
-                diffGrid.Rows.Add(points[i], values[i]);
-                diffGrid.Rows.Add();
-            }
+            Enumerable.Range(0, points.Count).ToList().ForEach(i => { diffGrid.Rows.Add(points[i], values[i]); diffGrid.Rows.Add(); });
                 
             int rowStartIndex = 1;
             int columnIndex = 2;
@@ -75,7 +65,7 @@ namespace InterpolationApp2
             }
         }
 
-        private void FillResultGrid(List<double> points, List<double> interpolatedValues, List<double> errors)
+        private void FillResultGrid(List<double> points, List<double> interpolatedValues, List<double> errors, List<double> maxModules, List<double> errorEstimations)
         {
             resultGrid.Rows.Add("Узлы");
             resultGrid.Rows.Add("P_i(x)");
@@ -88,108 +78,10 @@ namespace InterpolationApp2
                 resultGrid[i, 0].Value = points[i - 1];
                 resultGrid[i, 1].Value = interpolatedValues[i - 1];
                 resultGrid[i, 2].Value = errors[i - 1];
-
+                resultGrid[i, 3].Value = maxModules[i - 1];
+                resultGrid[i, 4].Value = errorEstimations[i - 1];
             }
         }
 
-        private List<List<double>> GetDividedDifferences(List<double> points, List<double> values)
-        {
-            var diffs = new List<List<double>>();
-
-            var diffs1 = new List<double>();
-            for (int i = 1; i < points.Count; i++)
-            {
-                var diff = (values[i] - values[i - 1]) / (points[i] - points[i - 1]);
-                diffs1.Add(diff);
-            }
-
-            diffs.Add(diffs1);
-
-            int diffNumber = points.Count / 2 - 1;
-            
-            for (int i = 0; i <= diffNumber; i++)
-            {
-                int beginPoint = 0;
-                int endPoint = i + 2;
-                var diffsN = new List<double>();
-
-                for (int j = 1; j < diffs[diffs.Count - 1].Count; j++)
-                {
-                    var diff = (diffs[diffs.Count - 1][j] - diffs[diffs.Count - 1][j - 1]) / (points[endPoint] - points[beginPoint]);
-                    diffsN.Add(diff);
-                    beginPoint++;
-                    endPoint++;
-                }
-
-                diffs.Add(diffsN);
-            }
-
-            return diffs;
-        }
-
-        private List<double> ChooseClosePoints(List<double> points, double intPoint, int count)
-        {
-            var newPoints = new List<double>();
-            
-            for (int i = 0; i < count; i++)
-            {
-                var distances = new List<double>();
-                points.ForEach(x => distances.Add(Math.Abs(x - intPoint)));
-                var minDistanceIndex = distances.IndexOf(distances.Min());
-
-                newPoints.Add(points[minDistanceIndex]);
-                points.Remove(points[minDistanceIndex]);
-            }
-
-            return newPoints;
-        }
-
-        private List<double> GetCalculationError(double value, List<double> calculatedValues)
-        {
-            return calculatedValues.Select(calcValue => value - calcValue).ToList();
-        }
-
-        private double Interpolate(double intPoint, List<double> points, List<double> values, int degree)
-        {
-            if (degree == 0)
-                return values[0];
-
-            var result = values[0];
-            var diffs = GetDividedDifferences(points, values);
-
-            for (int i = 0; i < degree; i++)
-            {
-                double product = 1;
-                for (int j = 0; j <= i; j++)
-                {
-                    product *= intPoint - points[j];
-                }
-
-                result += diffs[i][0] * product;
-            }
-
-            return result;
-        }
-
-        private double Differentiation(List<double> points, List<double> values, double diffPoint, int degree)
-        {
-            double recDifferentiation (List<double> newValues, int newDegree)
-            {
-                if (newDegree == 0)
-                {
-                    return CubicSpline.InterpolateNatural(points, newValues).Differentiate(diffPoint);
-                }
-                else
-                {
-                    var cs = CubicSpline.InterpolateNatural(points, newValues);
-                    newValues = points.Select(point => cs.Differentiate(point)).ToList();
-                    return recDifferentiation(newValues, newDegree - 1);
-                }
-            }
-
-            return recDifferentiation(values, degree);
-        }
-
-        //найти Mi+1 как максимум производной и погрешность
     }
 }
---
+++ Newton polynomial/InterpolationApp2/Interpolation.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics.Interpolation;
+
+namespace InterpolationApp2
+{
+    public static class Interpolation
+    {
+        public delegate double FunctionDelegate(double x);
+
+        public static List<List<double>> GetDividedDifferences(List<double> points, List<double> values)
+        {
+            var diffs = new List<List<double>>();
+
+            var diffs1 = new List<double>();
+            for (int i = 1; i < points.Count; i++)
+            {
+                var diff = (values[i] - values[i - 1]) / (points[i] - points[i - 1]);
+                diffs1.Add(diff);
+            }
+
+            diffs.Add(diffs1);
+
+            int diffNumber = points.Count / 2 - 1;
+
+            for (int i = 0; i <= diffNumber; i++)
+            {
+                int beginPoint = 0;
+                int endPoint = i + 2;
+                var diffsN = new List<double>();
+
+                for (int j = 1; j < diffs[diffs.Count - 1].Count; j++)
+                {
+                    var diff = (diffs[diffs.Count - 1][j] - diffs[diffs.Count - 1][j - 1]) / (points[endPoint] - points[beginPoint]);
+                    diffsN.Add(diff);
+                    beginPoint++;
+                    endPoint++;
+                }
+
+                diffs.Add(diffsN);
+            }
+
+            return diffs;
+        }
+
+        public static List<double> ChooseClosePoints(List<double> points, double intPoint, int count)
+        {
+            var newPoints = new List<double>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var distances = new List<double>();
+                points.ForEach(x => distances.Add(Math.Abs(x - intPoint)));
+                var minDistanceIndex = distances.IndexOf(distances.Min());
+
+                newPoints.Add(points[minDistanceIndex]);
+                points.Remove(points[minDistanceIndex]);
+            }
+
+            return newPoints;
+        }
+
+        public static List<double> GetCalculationError(double value, List<double> calculatedValues)
+        {
+            return calculatedValues.Select(calcValue => value - calcValue).ToList();
+        }
+
+        public static double Interpolate(double intPoint, List<double> points, List<double> values, int degree)
+        {
+            if (degree == 0)
+                return values[0];
+
+            var result = values[0];
+            var diffs = GetDividedDifferences(points, values);
+
+            for (int i = 0; i < degree; i++)
+            {
+                double product = 1;
+                for (int j = 0; j <= i; j++)
+                {
+                    product *= intPoint - points[j];
+                }
+
+                result += diffs[i][0] * product;
+            }
+
+            return result;
+        }
+
+
+        public static double Differentiate(List<double> points, List<double> values, double diffPoint, int degree)
+        {
+            double recDifferentiation(List<double> newValues, int newDegree)
+            {
+                if (newDegree == 0)
+                {
+                    return CubicSpline.InterpolateNatural(points, newValues).Differentiate(diffPoint);
+                }
+                else
+                {
+                    var cs = CubicSpline.InterpolateNatural(points, newValues);
+                    newValues = points.Select(point => cs.Differentiate(point)).ToList();
+                    return recDifferentiation(newValues, newDegree - 1);
+                }
+            }
+
+            return recDifferentiation(values, degree);
+        }
+
+        public static double EvaluateProductModule(List<double> points, List<double> values, double intPoint, int degree, double step)
+        {
+            var a = points.Concat(new List<double> { intPoint }).Min();
+            var b = points.Concat(new List<double> { intPoint }).Max();
+
+            var derivates = new List<double>();
+
+            for (double i = a; i < b; i += step)
+            {
+                var derivative = Differentiate(points, values, i, degree);
+                derivates.Add(Math.Abs(derivative));
+            }
+
+            return derivates.Max();
+        }
+
+        public static double EstimateError(List<double> points, List<double> values, double intPoint, int degree, double step)
+        {
+            var maxModule = EvaluateProductModule(points, values, intPoint, degree + 1, step);
+            var product = Math.Abs(points.Select(x => intPoint - x).Aggregate((acc, x) => acc * x));
+            var factorial = Enumerable.Range(1, degree + 1).Aggregate((i, r) => r * i);
+
+            return maxModule * product / factorial;
+        }
+
+    }
+}
