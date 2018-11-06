e5665886e7ec1f78e7af08365a1d1bc55018fda3
Dinara Faizullina <imfd@protonmail.com>
2018-11-06 20:49:13 +0300

    Started numerical integration

---
+++ Numerical Integration/InterpolationApp5/Integrals.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterpolationApp5
+{
+    public class Interval
+    {
+        public Interval(double left, double right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public double Left { get; set; }
+        public double Right { get; set; }
+    }
+
+    public static class Integrals
+    {
+        public delegate double FunctionDelegate(double x);
+
+        public static IEnumerable<double> Double(double from, double to, double h)
+        {
+            if (h <= 0.0) h = (h == 0.0) ? 1.0 : -h;
+
+            if (from <= to)
+            {
+                for (double d = from; d <= to; d += h) yield return d;
+            }
+            else
+            {
+                for (double d = from; d >= to; d -= h) yield return d;
+            }
+        }
+
+        private static double GetStep(Interval interval, double n)
+        {
+            return (interval.Right - interval.Left) / n;
+        }
+
+        public static double CalculateLeftRectangesIntegral(Interval interval, int n, FunctionDelegate f)
+        {
+            double h = GetStep(interval, n);
+            double sum = 0;
+            double x;
+
+            for (int i = 0; i < n; i++)
+            {
+                x = interval.Left + i * h;
+                sum += f(x);
+            }
+            return h * sum;
+        }
+
+        public static double CalcualteTrapezoidIntegral(Interval interval, int n, FunctionDelegate f)
+        {
+            double h = GetStep(interval, n);
+            double sum = 0;
+            double x;
+            
+            for (int i = 1; i < n - 1; i++)
+            {
+                x = interval.Left + i * h;
+                sum += f(x);
+            }
+
+            sum = sum * 2 + f(interval.Left) + f(interval.Right);
+            double coeff = (interval.Right - interval.Left) / 2 / n;
+            return  coeff * sum;
+        }
+
+        public static double CalculateSimpsonsIntergral(Interval interval, int n, FunctionDelegate f)
+        {
+            n *= 2;
+            double h = GetStep(interval, n);
+            double sum = 0;
+            double x;
+
+            for (int i = 1; i < n; i++)
+            {
+                x = interval.Left + i * h;
+                if (i % 2 == 0)
+                {
+                    sum += 2 * f(x);
+                }
+                else
+                {
+                    sum += 4 * f(x);
+                }
+            }
+
+            sum += f(interval.Left) + f(interval.Right);
+            double coeff = (interval.Right - interval.Left) / 3 / n;
+            return coeff * sum;
+        }
+
+        public static double GetError(double c, Interval interval, int n, int d, FunctionDelegate df)
+        {
+            var maxDf = GetMaxFunctionValue(interval, n, df);
+            return c * Math.Pow(interval.Right - interval.Left, d + 2) / Math.Pow(n, d + 1) * maxDf;
+        }
+
+        public static double GetErrorForLeftRectanges(Interval interval, int n, FunctionDelegate df)
+        {
+            return GetError(0.5, interval, n, 0, df);
+        }
+
+        public static double GetErrorForTrapezoidIntegral(Interval interval, int n, FunctionDelegate df)
+        {
+            return GetError(1 / 12, interval, n, 1, df);
+        }
+
+        public static double GetErrorForSimpsonsIntegral(Interval interval, int n, FunctionDelegate df)
+        {
+            return GetError(1 / 2880, interval, n, 3, df);
+        }
+
+        private static double GetMaxFunctionValue(Interval interval, int n, FunctionDelegate f)
+        {
+            return Double(interval.Left, interval.Right, (interval.Right - interval.Left) / n).Select(x => Math.Abs(f(x))).Max();
+        }
+
+        public static double GetMainError(double S2n, double Sn, int d)
+        {
+            return (S2n - Sn) / (Math.Pow(2, d + 1) - 1);
+        }
+
+        public static double GetMainErrorForLeftRectanges(double S2n, double Sn)
+        {
+            return GetMainError(S2n, Sn, 0);
+        }
+
+        public static double GetMainErrorForTrapezoidIntegral(double S2n, double Sn)
+        {
+            return GetMainError(S2n, Sn, 1);
+        }
+
+        public static double GetMainErrorForSimpsonsIntegral(double S2n, double Sn)
+        {
+            return GetMainError(S2n, Sn, 3);
+        }
+
+        public static double GetMainErrorForLeftRectanges(Interval interval, int n, FunctionDelegate f)
+        {
+            var Sn = CalculateLeftRectangesIntegral(interval, n, f);
+            var S2n = CalculateLeftRectangesIntegral(interval, 2 * n, f);
+            return GetMainErrorForLeftRectanges(S2n, Sn);
+        }
+
+        public static double GetMainErrorForTrapezoidIntegral(Interval interval, int n, FunctionDelegate f)
+        {
+            var Sn = CalcualteTrapezoidIntegral(interval, n, f);
+            var S2n = CalcualteTrapezoidIntegral(interval, 2 * n, f);
+            return GetMainErrorForTrapezoidIntegral(S2n, Sn);
+        }
+
+        public static double GetMainErrorForSimpsonsIntegral(Interval interval, int n, FunctionDelegate f)
+        {
+            var Sn = CalculateSimpsonsIntergral(interval, n, f);
+            var S2n = CalculateSimpsonsIntergral(interval, 2 * n, f);
+            return GetMainErrorForSimpsonsIntegral(S2n, Sn);
+        }
+
+        public static double GetAdjustedIntegral(double S2n, double Rmain)
+        {
+            return S2n + Rmain;
+        }
+    }
+}
---
+++ Numerical Integration/InterpolationApp5/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace InterpolationApp5
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MethodColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SnColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IntergralSnDDiffColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RnColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.S2nColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IntergralS2nDiffColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.R2nColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RmainColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IAdjustedColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IntergralIAdjustedDiffColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MethodColumn,
+            this.SnColumn,
+            this.IntergralSnDDiffColumn,
+            this.RnColumn,
+            this.S2nColumn,
+            this.IntergralS2nDiffColumn,
+            this.R2nColumn,
+            this.RmainColumn,
+            this.IAdjustedColumn,
+            this.IntergralIAdjustedDiffColumn});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MethodColumn
+            // 
+            this.MethodColumn.HeaderText = "Метод";
+            this.MethodColumn.Name = "MethodColumn";
+            // 
+            // SnColumn
+            // 
+            this.SnColumn.HeaderText = "Sn";
+            this.SnColumn.Name = "SnColumn";
+            // 
+            // IntergralSnDDiffColumn
+            // 
+            this.IntergralSnDDiffColumn.HeaderText = "I - Sn";
+            this.IntergralSnDDiffColumn.Name = "IntergralSnDDiffColumn";
+            // 
+            // RnColumn
+            // 
+            this.RnColumn.HeaderText = "Rn";
+            this.RnColumn.Name = "RnColumn";
+            // 
+            // S2nColumn
+            // 
+            this.S2nColumn.HeaderText = "S2n";
+            this.S2nColumn.Name = "S2nColumn";
+            // 
+            // IntergralS2nDiffColumn
+            // 
+            this.IntergralS2nDiffColumn.HeaderText = "I - S2n";
+            this.IntergralS2nDiffColumn.Name = "IntergralS2nDiffColumn";
+            // 
+            // R2nColumn
+            // 
+            this.R2nColumn.HeaderText = "R2n";
+            this.R2nColumn.Name = "R2nColumn";
+            // 
+            // RmainColumn
+            // 
+            this.RmainColumn.HeaderText = "Rmain";
+            this.RmainColumn.Name = "RmainColumn";
+            // 
+            // IAdjustedColumn
+            // 
+            this.IAdjustedColumn.HeaderText = "Iad";
+            this.IAdjustedColumn.Name = "IAdjustedColumn";
+            // 
+            // IntergralIAdjustedDiffColumn
+            // 
+            this.IntergralIAdjustedDiffColumn.HeaderText = "I - Iad";
+            this.IntergralIAdjustedDiffColumn.Name = "IntergralIAdjustedDiffColumn";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MethodColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SnColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IntergralSnDDiffColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RnColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn S2nColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IntergralS2nDiffColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn R2nColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RmainColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IAdjustedColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IntergralIAdjustedDiffColumn;
+    }
+}
+
